1af006640609b0a80dd49d49864adc38fa4670b1
Hidemitsu Shmizu <hide0917>
2021-09-13 02:29:31 +0900

    first commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Hidemitsu Shmizu on 2021/09/13.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
